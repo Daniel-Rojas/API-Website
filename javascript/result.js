55b217122dee3e10abe58c2bc4692dd8c8cd18f8
Daniel Rojas <daniel.rojas913@gmail.com>
2018-11-25 00:03:11 -0600

    Added Icons/forecast HTML/weather api
    weather api is commented and has not been tested

--- javascript/result.js
+++ javascript/result.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
 
   document.getElementById("current-city").innerHTML += userData.currentCity;
   loadCurrentMap(userData.currentCity);
+  //loadCurrentForecast(userData.currentCity.cityName);
 
   document.getElementById("destination-city").innerHTML +=
     userData.destinationCity;
   loadDestinationMap(userData.destinationCity);
+  //loadDestinationForecast(userData.destinationCity.cityName);
 });
 
 function loadCurrentMap(currentCity) {
@@ -34,4 +36,92 @@ function loadDestinationMap(destinationCity) {
   destinationPin.getGeocodingInfo();
   destinationMap.addPin(destinationPin);
   destinationMap.map.setCenter(destinationPin.position);
+}
+
+function loadCurrentForecast(currentCity) {
+  let currentCityWeather = WeeklyForecast(currentCity);
+  currentCityWeather.getWeatherForecast();
+  for (let i = 0; i < currentCityWeather.forecast.length(); i++) {
+    // set html values
+  }
+
+}
+
+function loadDestinationForecast(destinationCity) {
+
+}
+
+class WeeklyForecast {
+  constructor(location) {
+    this.forecast = [];
+    this.location = location;
+    this.locationKey = this.getLocationKey();
+  }
+
+  getLocationKey() {
+    let apiUrl = "http://dataservice.accuweather.com/locations/v1/search";
+    let apiKey = "tCZnjOflxGgwGubWXbpj3yhbOS2kZXdZ";
+    let parameters = { apiKey: apiKey, q: this.location };
+
+    var tempLocationKey;
+
+    $.get({
+      url: apiUrl,
+      data: parameters,
+      async: false,
+      success: function(response) {
+        tempLocationKey = response[0].Key;
+      }
+    });
+    return tempLocationKey;
+  }
+
+  getWeatherForecast() {
+    let apiUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + this.locationKey;
+    let apiKey = "tCZnjOflxGgwGubWXbpj3yhbOS2kZXdZ";
+    let details = "true";
+    let parameters = { apiKey: apiKey, details: details }
+
+    var tempForecast = [];
+
+    $.get({
+      url: apiUrl,
+      data: parameters,
+      async: false,
+      success: function(response) {
+        for (let i = 0; i < 5; i++) {
+          let data = response.DailyForecasts[i];
+          let day = new DayForecast();
+          day.getForecastInfo(data);
+          tempforecast[i] = day;
+        }
+      }
+    });
+    this.forecast = tempForecast;
+  }
+}
+
+class DayForecast {
+  constructor() { 
+    this.date;
+    this.condition;
+    this.icon;
+    this.minTemperature;
+    this.maxTemperature;
+    this.precipitation;
+    this.windSpeed;
+    this.windDirection;
+  }
+
+  getForecastInfo(data) {
+    this.date = data.Date;
+    this.minTemperature = data.Temperature.Minimum.Value;
+    this.maxTemperature = data.Temperature.Maximum.Value;
+    this.condition = data.Day.IconPhrase;
+    this.precipitation = data.Day.PrecipitationProbability;
+    this.windSpeed = data.Day.Wind.Speed;
+    this.windDirection = data.Day.Wind.Direction.Localized;
+    let iconNumber = data.Day.Icon;
+    this.icon = "../images/icons/weather/" + String(iconNumber) + ".png";
+  }
 }
